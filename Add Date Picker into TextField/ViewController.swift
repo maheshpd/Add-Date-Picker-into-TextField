f4b324c4cdddf341f3440ffe70b373b860b83715
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-05-26 14:30:05 +0530

    done

--- Add Date Picker into TextField/ViewController.swift
+++ Add Date Picker into TextField/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    var datePicker = UIDatePicker()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createDatePicker()
     }
 
-
+    func createDatePicker() {
+        datePicker.datePickerMode = .dateAndTime
+        let tollbar = UIToolbar()
+        tollbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneac))
+        
+        tollbar.setItems([doneButton], animated: true)
+        dateTextField.inputAccessoryView = tollbar
+    }
+    
+    @objc func doneac() {
+        dateTextField.text = "\(datePicker.date)"
+        self.view.endEditing(true)
+    }
+    
 }
 
